15c108e6e47142ade9edb5ec063b3dcedb11cb7e
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-02-28 06:19:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, obj, [key])
+  var newObj = Object.assign({}, object)
   delete newObj[key]
   return newObj
 }
